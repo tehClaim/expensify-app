96b40987dc42c59f867181c3e7dc04ed3a2900b4
ksh <exclaimz@gmail.com>
2019-03-26 21:57:44 +0300

    Setup test database env

---
+++ src/firebase/firebase.js
@@ -0,0 +1,142 @@
+import * as firebase from 'firebase';
+
+const config = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+};
+
+firebase.initializeApp(config);
+
+const database = firebase.database();
+
+export { firebase, database as default };
+
+// database.ref('expenses').on('child_removed', (snapshot) => {
+//   console.log(snapshot.key, snapshot.val());
+// });
+
+// database.ref('expenses').on('child_changed', (snapshot) => {
+//   console.log(snapshot.key, snapshot.val());
+// });
+
+// database.ref('expenses').on('child_added', (snapshot) => {
+//   console.log(snapshot.key, snapshot.val());
+// });
+
+// database.ref('expenses')
+//   .once('value')
+//   .then((snapshot) => {
+//     const expenses = [];
+
+//     snapshot.forEach((childSnapshot) => {
+//       expenses.push({
+//         id: childSnapshot.key,
+//         ...childSnapshot.val()
+//       })
+//     });
+
+//     console.log(expenses);
+//   });
+
+
+// database.ref('expenses').on('value', (snapshot) => {
+//   const expenses = [];
+
+//   snapshot.forEach((childSnapshot) => {
+//     expenses.push({
+//       id: childSnapshot.key,
+//       ...childSnapshot.val()
+//     })
+//   });
+//   console.log('my expenses', expenses);
+// });
+
+
+// database.ref('expenses').push({
+//   description: 'Rent',
+//   note: '',
+//   amount: 109500,
+//   createdAt: '97894654654'
+// });
+
+
+
+
+// const notes = [{
+//   id: '12',
+//   title: 'First note',
+//   body: 'This is my note'
+// }, {
+//   id: '761ase',
+//   title: 'Another Note',
+//   body: 'This is my note'
+// }];
+
+// database.ref('notes').set(notes);
+
+// const onValueChange = database.ref().on('value', (snapshot) => {
+//   const val = snapshot.val();
+//   console.log(`${val.name} is a ${val.job.title} at ${val.job.company}`);
+// });
+
+
+// setTimeout(() => {
+//   database.ref('age').set(29);
+// }, 3500);
+
+// setTimeout(() => {
+//   database.ref().off();
+// }, 7000);
+
+// setTimeout(() => {
+//   database.ref('age').set(30);
+// }, 10500);
+
+// database.ref('location/city')
+//   .once('value')
+//   .then((snapshot) => {
+//     const val = snapshot.val();
+//     console.log(val);
+//   })
+//   .catch((e) => {
+//     console.log('Error fetching data', e);
+//   });
+
+// database.ref().set({
+//   name: 'Ur name right there',
+//   age: 28,
+//   stressLevel: 6,
+//   job: {
+//     title: 'Software developer',
+//     company: 'Google'
+//   },
+//   location: {
+//     city: 'Moscow',
+//     country: 'Russia'
+//   }
+// }).then(() => {
+//   console.log('Data is saved!');
+//   database.ref().update({
+//     stressLevel: 9,
+//     'job/company': 'Amazon',
+//     'location/city': 'Seattle'
+//   });
+// }).catch((e) => {
+//   console.log('This failed', e);
+// });
+
+
+
+//database.ref('isSingle').set(null)
+
+// database.ref('isSingle').remove()
+//   .then(() => {
+//     console.log('removed successfully');
+//   })
+//   .catch((e) => {
+//     console.log('Something went wrong', e);
+//   });
--- webpack.common.js
+++ webpack.common.js
@@ -2,6 +2,15 @@ const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const webpack = require('webpack');
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+if (process.env.NODE_ENV === 'test') {
+  require('dotenv').config({ path: '.env.test'});
+} else if (process.env.NODE_ENV === 'development') {
+  require('dotenv').config({ path: '.env.dev'});
+}
 
 module.exports = {
   entry: {
@@ -16,6 +25,14 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: "[name].css",
       chunkFilename: "[id].css"
+    }),
+    new webpack.DefinePlugin({
+      'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
+      'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+      'process.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.FIREBASE_DATABASE_URL),
+      'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
+      'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+      'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID)
     })
   ], 
   output: {
