96d933790056983b00ea5769036ceb0510c3ce0e
ksh <exclaimz@gmail.com>
2019-03-29 17:36:40 +0300

    Setup private route

--- src/app.js
+++ src/app.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import AppRouter from './routers/AppRouter';
+import AppRouter, { history } from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { startSetExpenses } from './actions/expenses';
+import { login, logout } from './actions/auth';
 import 'normalize.css/normalize.css'
 import './styles/styles.scss';
 import 'react-dates/initialize';
 import 'react-dates/lib/css/_datepicker.css';
-import './firebase/firebase';
+import { firebase } from './firebase/firebase';
 
 const store = configureStore();
 const jsx = (
@@ -16,14 +17,34 @@ const jsx = (
     <AppRouter />
   </Provider>
 );
+let hadRendered = false;
+const renderApp = () => {
+  if (!hadRendered) {
+    ReactDOM.render(jsx, document.getElementById('app'));
+    hadRendered = true;
+  }
+};
 
 const element = document.createElement('div');
 element.setAttribute('id', 'app');
 document.body.appendChild(element);
 
 
-ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
+ReactDOM.render(jsx, document.getElementById('app'));
 
-store.dispatch(startSetExpenses()).then(() => {
-  ReactDOM.render(jsx, document.getElementById('app'));
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    store.dispatch(login(user.uid));
+    store.dispatch(startSetExpenses()).then(() => {
+      renderApp();
+      if (history.location.pathname === '/') {
+        history.push('/dashboard');
+      }
+    });
+  } else {
+    store.dispatch(logout());
+    renderApp();
+    history.push('/');
+
+  }
 });
